7a081f6d3f1a84fc9da512c95176533cd0219528
Julián Eduardo Aguirre <aguirre.julian.eduardo@gmail.com>
2023-03-03 17:18:37 -0300

    Rearranged card-generate functions. Starting details func

--- assets/js/common.js
+++ assets/js/common.js
@@ -5,17 +5,18 @@ let buttonClassSelector = (fecha1, fecha2) => {
 	if (fecha1 < fecha2){
 		btnClass = '"btn btn-outline-info"'
 	}
-
+	
 	return btnClass;
 }
 
+const buttonClassSelector2 = (fecha1, fecha2) => {return fecha1<=fecha2 ? '"btn btn-outline-info"' : '"btn btn-outline-secondary"' }
 
 
-// FUNCION PARA OBTENER TODAS LAS CATEGORIAS DENTRO DEL OBJETO DATA
+
+// SET con las categorías encontradas en la información de los eventos
 const categorySetGenerator = (data) => {
 	let categoriesSet = new Set();
 	data.events.forEach( evento => categoriesSet.add(evento.category) )
-	// console.log(categoriesSet)
 	return categoriesSet;
 }
 
@@ -40,6 +41,62 @@ const checkBoxGenerator = (data) => {
 
 	return checkBoxHTML;
 }
+
+
+
+// NUEVAS FUNCIONES 03/03/23
+function viewDetails(card_id) {
+	window.location.href = `./details.html?id=${card_id}`
+}
+
+
+
+
+// Solo realizo una tarjeta, que puedo concatenar con las demás
+// Args: evento (objeto con los datos del evento)
+// 			clase_boton (clase BS5 en base a la fecha del evento, uso la funcion buttonClassSelector)
+function generar_tarjeta(evento, clase_boton){
+	// console.log("Dentro del generador de tarjetas")
+	// console.log(evento.id)
+	// console.log(evento._id)
+	return `<div class="col-xs-10 col-sm-6 col-md-5 col-lg-4 col-xl-3 col-xxl-3 mb-4 d-flex justify-content-center">
+	<div class="card h-100">
+		<img src="${evento.image}" class="card-img-top" alt="${evento.name} image">
+		<div class="card-body">
+		<h5 class="card-title text-center">${evento.name}</h5>
+		<div class="d-flex mb-0 justify-content-evenly">
+			<p class="card-price d-inline mb-0"><small>${evento.date}</small></p>
+			<p class="card-price d-inline mb-0"><strong>$${evento.price}</strong></p>
+		</div>
+		<hr class="mb-2 mt-2">
+		<p class="card-text mb-2">${evento.description}</p>
+		</div>
+		<div class="card-footer">
+			<btn onclick="viewDetails(${evento._id})" class=${clase_boton}>View Details</btn>
+		</div>
+	</div>
+</div>
+`
+}
+
+
+// FUNCION FINAL PARA GENERAR TARJETAS EN FORMA DINAMICA
+function generar_tarjetas2(eventos, fechaRef){
+
+	let html_tarjetas = `<div class="row mt-5 justify-content-around mb-5">`
+
+	if (eventos.length != 0){
+		eventos.forEach((evento) => {
+			html_tarjetas += generar_tarjeta(evento, buttonClassSelector2(fechaRef, evento.date))
+		})
+	} else {
+		html_tarjetas += `<h1 class="text-center" style="color:white;font-size:3rem;">Ups, no coincidences!</h1>`
+	}
+
+	return html_tarjetas + `</div>`
+}
+
+
 
 
 
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,62 +1,28 @@
 
-
-// OBLIGADAMENTE LE ESTOY PASANDO TODO EL OBJETO DATA
-function generar_tarjetas(data){
-	let html_tarjetas = `<div class="row mt-5 justify-content-around mb-5">`;
-	console.log(data.events)
-	if (data.events.lenght != []){
-		console.log("CON LENGHT")
-
-		for (const evento of data.events) {
-		
-			let btnClass = buttonClassSelector(new Date(data.currentDate), new Date(evento.date));
-	
-			html_tarjetas += `<div class="col-xs-10 col-sm-6 col-md-5 col-lg-4 col-xl-3 col-xxl-3 mb-4 d-flex justify-content-center">
-					<div class="card h-100">
-						<img src="${evento.image}" class="card-img-top" alt="${evento.name} image">
-						<div class="card-body">
-						<h5 class="card-title text-center">${evento.name}</h5>
-						<div class="d-flex mb-0 justify-content-evenly">
-							<p class="card-price d-inline mb-0"><small>${evento.date}</small></p>
-							<p class="card-price d-inline mb-0"><strong>$${evento.price}</strong></p>
-						</div>
-						<hr class="mb-2 mt-2">
-						<p class="card-text mb-2">${evento.description}</p>
-						</div>
-						<div class="card-footer">
-							<a href="./details.html" class=${btnClass}>View Details</a>
-						</div>
-					</div>
-				</div>
-				`
-		}
-
-	} else {
-		console.log("SIN LENGHT")
-		html_tarjetas += `<h2>No coincidences</h2>`
-	}
-	html_tarjetas += `</div>`
-	return html_tarjetas;
-}
-
-
-// Contenedor de tarjetas
+// SECCION CARDS
 const div_tarjetas = document.getElementById("cartas");
 //Si tiene contenido, lo elimino primero
 while(div_tarjetas.firstChild) {
 					div_tarjetas.removeChild(div_tarjetas.firstChild);
-			}
+}
 
-div_tarjetas.innerHTML = generar_tarjetas(data);
+// const nuevo = {
+// 	currentDate: data.currentDate,
+// 	events: []
+// }
 
 
-// PARA EL SEACH BAR
-// Contenedor de checkboxes
+div_tarjetas.innerHTML = generar_tarjetas2(data.events, data.currentDate);
+
+
+// SECCION SEARCH FILTER (CHECKBOXES & INPUT)
+
+// CHECKBOX CONTAINER
 const div_checkboxes = document.getElementById("checkboxes")
 while(div_checkboxes.firstChild) {
 	div_checkboxes.removeChild(div_checkboxes.firstChild);
 }
-// Desde common.js
+// CHECKBOX GENERATOR (from common.js)
 div_checkboxes.innerHTML = checkBoxGenerator(data);
 
 
@@ -67,29 +33,21 @@ div_checkboxes.innerHTML = checkBoxGenerator(data);
 // div_tarjetas.innerHTML = generar_tarjetas(data);
 
 const inputSearch = document.getElementById("search")
-// console.log(inputSearch)
 inputSearch.addEventListener("input", () => {
 	let eventosFiltrados = data.events.filter((evento) => evento.name.toLowerCase().includes(inputSearch.value.toLowerCase()))
-	// console.log(eventosFiltrados)
-	// console.log(eventosFiltrados.length)
 
-	let new_data = {
-		currentDate: data.currentDate,
-		events: eventosFiltrados	
-	}
 
-	// console.log(new_data.events.length)
+	// Contenedor de tarjetas
+	const div_tarjetas = document.getElementById("cartas");
+	//Si tiene contenido, lo elimino primero
+	while(div_tarjetas.firstChild) {
+						div_tarjetas.removeChild(div_tarjetas.firstChild);
+				}
 
-// Contenedor de tarjetas
-const div_tarjetas = document.getElementById("cartas");
-//Si tiene contenido, lo elimino primero
-while(div_tarjetas.firstChild) {
-					div_tarjetas.removeChild(div_tarjetas.firstChild);
-			}
-
-div_tarjetas.innerHTML = generar_tarjetas(new_data);
+	div_tarjetas.innerHTML = generar_tarjetas2(eventosFiltrados, data.currentDate);
 
 
 })
+
 
 
