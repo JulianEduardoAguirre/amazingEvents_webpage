d675fb81e2e08518a92fa423e6211240658f581e
Julián Eduardo Aguirre <aguirre.julian.eduardo@gmail.com>
2023-03-14 10:26:11 -0300

    Refactor in common and cards pages

--- assets/js/common.js
+++ assets/js/common.js
@@ -3,6 +3,12 @@ let apiUrl = "https://mindhub-xj03.onrender.com/api/amazing"
 let localData = "./assets/amazing_1.json";
 
 
+// GO TO DETAILS
+function viewDetails(card_id) {
+	window.location.href = `./details.html?id=${card_id}`
+}
+
+
 // SET WITH THE CATEGORIES
 const categorySetGenerator = (allEvents) => {
 	let categoriesSet = new Set();
@@ -32,14 +38,8 @@ const checkBoxGenerator = (events) => {
 }
 
 
-// GO TO DETAILS
-function viewDetails(card_id) {
-	window.location.href = `./details.html?id=${card_id}`
-}
-
-
-// SIMGLE CARD GENERATOR
-function generate_card(evento, refDate){
+// SINGLE CARD GENERATOR
+function generateCard(evento, refDate){
 
 	return `<div class="mb-4 d-flex justify-content-center" onclick="viewDetails(${evento._id})">
 	<div class="card h-100">
@@ -63,13 +63,13 @@ function generate_card(evento, refDate){
 
 
 // ALL-CARDS GENERATOR FUNCTION
-function generar_tarjetas2(events, refDate){
+function generateCards(events, refDate){
 
 	let cardsHTML = `<div class="d-flex flex-wrap my-5 justify-content-around">`
 
 	if (events.length != 0){
 		events.forEach((event) => {
-			cardsHTML += generate_card(event, refDate)
+			cardsHTML += generateCard(event, refDate)
 		})
 	} else {
 		cardsHTML += `<div class="d-flex flex-column"> <p class="text-center" style="color:white;font-size:3rem;">Oops, no coincidences!</p><p class="text-center" style="color:white;font-size:2rem;">Try adjusting your search parameters</p></div>`
@@ -80,7 +80,7 @@ function generar_tarjetas2(events, refDate){
 
 
 // FILTER BASED ON FILTER SECTION
-function filterContent() {
+function filterContent(my_object) {
 	// Checkboxes (only selected)
 	let categoriesFound = [];
 	document.querySelectorAll(".form-check-input").forEach( e => {if(e.checked == true) categoriesFound.push(e.value)})
@@ -88,32 +88,7 @@ function filterContent() {
 	// Input search current value
 	let searchWord = document.getElementById("search").value.toLowerCase();
 	
-	let filteredEvents = filterByDate()		//Initially, got all events
-
-	// data object filter section
-	if ( categoriesFound.length != 0){
-		filteredEvents = filteredEvents.filter( evento => categoriesFound.includes(evento.category))
-	}
-
-	if (searchWord != ""){
-		filteredEvents = filteredEvents.filter( evento => evento.name.toLowerCase().includes(searchWord))
-	}
-
-	const div_tarjetas = document.getElementById("cartas");
-	div_tarjetas.innerHTML = generar_tarjetas2(filteredEvents, data.currentDate);
-}
-
-
-// FILTER BASED ON FILTER SECTION
-function filterContent2(my_object) {
-	// Checkboxes (only selected)
-	let categoriesFound = [];
-	document.querySelectorAll(".form-check-input").forEach( e => {if(e.checked == true) categoriesFound.push(e.value)})
-
-	// Input search current value
-	let searchWord = document.getElementById("search").value.toLowerCase();
-	
-	let filteredEvents = filterByDate2(my_object.currentTarget.myParam)																				//Initially, got all events
+	let filteredEvents = filterByDate(my_object.currentTarget.myParam)																				//Initially, got all events
 
 	// data object filter section
 	if ( categoriesFound.length != 0){
@@ -125,27 +100,11 @@ function filterContent2(my_object) {
 	}
 
 	const cards_div = document.getElementById("cartas");
-	cards_div.innerHTML = generar_tarjetas2(filteredEvents, my_object.currentTarget.myParam.currentDate);
+	cards_div.innerHTML = generateCards(filteredEvents, my_object.currentTarget.myParam.currentDate);
 }
 
 
-// FILTER BASED ON DATE (BASED ON CURRENT URL)           NOT USED
-function filterByDate() {
-	
-	let currentURL = window.location.href
-	let eventsFilteredByDate = data.events
-	if( currentURL.includes("upcoming")){
-		eventsFilteredByDate = data.events.filter( e => e.date >= data.currentDate)
-	} else if (currentURL.includes("past")){
-		eventsFilteredByDate = data.events.filter( e => e.date < data.currentDate)
-	}
-
-	return eventsFilteredByDate;
-}
-
-
-
-function filterByDate2(my_object) {
+function filterByDate(my_object) {
 
 	let currentURL = window.location.href
 	let eventsFilteredByDate = my_object.events
@@ -199,24 +158,19 @@ function generateStats(dataObject) {
 	// STATS BY CATEGORY (FUTURE EVENTS AND PAST EVENTS)
 
 	// Arranged to store categories for each time period (upcomind or past)
-	// let upcomingCategories = catArrayGenerator(dataObject.events.filter( e => dataObject.currentDate >= e.date));
-	let upcomingCategories = categorySetGenerator(dataObject.events.filter( e => dataObject.currentDate >= e.date));
-	console.log(upcomingCategories)
-	let pastCategories = categorySetGenerator(dataObject.events.filter( e => dataObject.currentDate < e.date));
+	let upcomingCategories = categorySetGenerator(dataObject.events.filter( e => dataObject.currentDate <= e.date));
+	let pastCategories = categorySetGenerator(dataObject.events.filter( e => dataObject.currentDate > e.date));
 
 
 	let upcomingEventsStats = []
-	upcomingCategories.forEach( catName => upcomingEventsStats.push(generateCategoryStats(catName, dataObject.events.filter(event => dataObject.currentDate >= event.date))) )
+	upcomingCategories.forEach( catName => upcomingEventsStats.push(generateCategoryStats(catName, dataObject.events.filter(event => dataObject.currentDate <= event.date))) )
 
 	let pastEventsStats = []
-	pastCategories.forEach( catName => pastEventsStats.push(generateCategoryStats(catName, dataObject.events.filter(event => dataObject.currentDate < event.date))) )
+	pastCategories.forEach( catName => pastEventsStats.push(generateCategoryStats(catName, dataObject.events.filter(event => dataObject.currentDate > event.date))) )
 
-
+	// RETURNED OBJECT
 	let allStats = {
 		cota: mayorLenght,
-		// maxCap: arrayEqualizer(mayorLenght, majorCapacity),
-		// maxPerc: arrayEqualizer(mayorLenght, arrayMaximunAtt),
-		// minPerc: arrayEqualizer(mayorLenght, arrayMinimunAtt),
 		maxCap: majorCapacity,
 		maxPerc: arrayMaximunAtt,
 		minPerc: arrayMinimunAtt,
@@ -228,16 +182,6 @@ function generateStats(dataObject) {
 
 }
 
-
-// Categories Array Generator (NOT USED?)
-
-// let catArrayGenerator = (eventsArray) => {
-// 	let catSet = new Set();
-// 	eventsArray.forEach(event => catSet.add(event.category))
-// 	return [...catSet]
-// }
-
-
 // Generic category stats generator
 let generateCategoryStats = (categoryName, eventsArray) => {
 	let categoryData = {
@@ -246,6 +190,7 @@ let generateCategoryStats = (categoryName, eventsArray) => {
 		capacityTotal: 0,
 		revenue: 0,
 	}
+
 	eventsArray.filter( event => {if(event.category == categoryName) {
 		categoryData.attendanceTotal += event.hasOwnProperty("assistance") ? event.assistance : event.estimate;
 		categoryData.capacityTotal += event.capacity
@@ -256,18 +201,39 @@ let generateCategoryStats = (categoryName, eventsArray) => {
 }
 
 
-// Generates a new array with it's original content and filling the rest of its values with an empty string  (ANOTHER NOT USED!!)
-// let arrayEqualizer = function(cota, arrToEq) {
-// 	let arrRtn = []
-// 	for (let index = 0; index < cota; index++) {
-// 		if (arrToEq[index] === undefined){
-// 			arrRtn.push("")
-// 		} else {
-// 			arrRtn.push(arrToEq[index])
-// 		}
-		
-// 	}
-// 	return arrRtn;	
-// }
+// ****************************************** NOT USED ****************************************************
+// FILTER BASED ON FILTER SECTION (PROTOTYPE)
+function filterContentProtorype() {
+	let categoriesFound = [];
+	document.querySelectorAll(".form-check-input").forEach( e => {if(e.checked == true) categoriesFound.push(e.value)})
+
+	let searchWord = document.getElementById("search").value.toLowerCase();
+	
+	let filteredEvents = filterByDate()		//Initially, got all events
+
+	if ( categoriesFound.length != 0){
+		filteredEvents = filteredEvents.filter( evento => categoriesFound.includes(evento.category))
+	}
+
+	if (searchWord != ""){
+		filteredEvents = filteredEvents.filter( evento => evento.name.toLowerCase().includes(searchWord))
+	}
+
+	const div_tarjetas = document.getElementById("cartas");
+	div_tarjetas.innerHTML = generateCards(filteredEvents, data.currentDate);
+}
 
 
+// FILTER BASED ON DATE (BASED ON CURRENT URL) (PROTYPE)
+function filterByDatePrototype() {
+	
+	let currentURL = window.location.href
+	let eventsFilteredByDate = data.events
+	if( currentURL.includes("upcoming")){
+		eventsFilteredByDate = data.events.filter( e => e.date >= data.currentDate)
+	} else if (currentURL.includes("past")){
+		eventsFilteredByDate = data.events.filter( e => e.date < data.currentDate)
+	}
+
+	return eventsFilteredByDate;
+}
--- assets/js/upcoming.js
+++ assets/js/upcoming.js
@@ -12,7 +12,7 @@ let asyncIndex = async function(){
 		const div_tarjetas = document.getElementById("cartas");
 		while(div_tarjetas.firstChild) {div_tarjetas.removeChild(div_tarjetas.firstChild);}	  			//Delete previous content (if exists)
 
-		div_tarjetas.innerHTML = generar_tarjetas2(filterByDate2(apiEvents), apiEvents.currentDate);								//First time rendering cards (using all data)
+		div_tarjetas.innerHTML = generateCards(filterByDate(apiEvents), apiEvents.currentDate);								//First time rendering cards (using all data)
 
 
 		// FILTER SECTION (CHECKBOXES & INPUT)
@@ -21,19 +21,19 @@ let asyncIndex = async function(){
 		const div_checkboxes = document.getElementById("checkboxes")
 		while(div_checkboxes.firstChild) { div_checkboxes.removeChild(div_checkboxes.firstChild);}	//Delete previous content (if exists)
 
-		div_checkboxes.innerHTML = checkBoxGenerator(filterByDate2(apiEvents));																//First (and unique) time rendering checkboxes
+		div_checkboxes.innerHTML = checkBoxGenerator(filterByDate(apiEvents));																//First (and unique) time rendering checkboxes
 
 
 
 		//																												ADDING EVENT LISTENERS FOR FILTER SECTION
 
 		const inputSearch = document.getElementById("search")
-		inputSearch.addEventListener("input", filterContent2)
+		inputSearch.addEventListener("input", filterContent)
 		inputSearch.myParam = apiEvents;
 
 		const otrosCheckboxes = document.querySelectorAll(".form-check-input")
 		for (const checkbox of otrosCheckboxes) {
-			checkbox.addEventListener("click", filterContent2);
+			checkbox.addEventListener("click", filterContent);
 			checkbox.myParam = apiEvents;
 		}		
 
